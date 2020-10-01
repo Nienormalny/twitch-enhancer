8b9db19b9413634bde13dccf10ca81c16041e3fd
HitoIRL <hitoirl1@gmail.com>
2020-10-01 20:05:21 +0200

    Zmieniona nazwa z :d na pepelaugh

--- src/utils/emotes.js
+++ src/utils/emotes.js
@@ -41,7 +41,7 @@ export const emotes = (emoji) => {
             return {
                 status: true, 'emoji': 'https://static-cdn.jtvnw.net/emoticons/v2/9/default/light/3.0'
             };
-        case '.':
+        case 'pepelaugh':
             return {
                 status: true, 'emoji': pepelaugh
             };

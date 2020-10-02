a0c4dc4e85e129d353e31b9cad1e26bcbab8acad
Damian <hitoirl1@gmail.com>
2020-10-03 01:56:30 +0200

    Naprawione błędy

--- src/utils/emotes.js
+++ src/utils/emotes.js
@@ -2,8 +2,11 @@ import { GETRequest } from './functions.js'
 import { isEmpty } from 'lodash';
 
 var loadedEmotes = {};
+var notEmotes = {};
 
 const GetEmoji = (emoji) => {
+    if (notEmotes[emoji])
+        return false;
     if (loadedEmotes[emoji])
         return true; // emote already loaded
 
@@ -12,25 +15,39 @@ const GetEmoji = (emoji) => {
     var parsed = JSON.parse(request);
     if (!isEmpty(parsed['emoticons']))
     {
-        var highestDPI = Object.keys(parsed['emoticons'][0]['urls']).length;
-        if (highestDPI === 3) // w pelni profesjonalny kod którego nawet cukierberg nie potrafi zrobić XDD
-            highestDPI++; // XDDDD
-        loadedEmotes[emoji] = parsed['emoticons'][0]['urls'][highestDPI];
-        console.log('Adding FFZ Emote ' + emoji);
-        return true;
+        for (var i = 0; i < parsed['emoticons'].length; i++)
+        {
+            if (parsed['emoticons'][i]['name'] !== emoji)
+                continue;
+            
+            var highestDPI = Object.keys(parsed['emoticons'][i]['urls']).length;
+            if (highestDPI === 3) // w pelni profesjonalny kod którego nawet cukierberg nie potrafi zrobić XDD
+                highestDPI++; // XDDDD
+            loadedEmotes[emoji] = parsed['emoticons'][i]['urls'][highestDPI];
+            console.log('Adding FFZ Emote ' + emoji);
+            return true;
+        }
     }
 
     // check if emoji exists in bttv
     // TODO: bttv global emotes
-    request = GETRequest("https://api.betterttv.net/3/emotes/shared/search?query=alliecatjam&offset=0&limit=50");
+    request = GETRequest("https://api.betterttv.net/3/emotes/shared/search?query=" + emoji + "&offset=0&limit=50");
     parsed = JSON.parse(request);
     if (!isEmpty(parsed))
     {
-        loadedEmotes[emoji] = "//cdn.betterttv.net/emote/" + parsed[0]['id'] + "/3x";
-        console.log('Adding BTTV Emote ' + emoji);
-        return true;
+        for (i = 0; i < parsed.length; i++) 
+        {
+            if (parsed[i]['code'] !== emoji)
+                continue;
+
+            loadedEmotes[emoji] = "//cdn.betterttv.net/emote/" + parsed[i]['id'] + "/3x";
+            console.log('Adding BTTV Emote ' + emoji);
+            return true;
+        }
     }
 
+    console.log('Adding not emote ' + emoji);
+    notEmotes[emoji] = true;
     return false;
 }
 
